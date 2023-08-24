4d80a8f65c4e3e3cd472d503cf37c7ac8e3316da
omarmohfaheem <omarmohamedfaheem@gmail.com>
2023-08-24 13:35:09 +0300

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
+const cors = require("cors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(cors({ credentials: true, origin: "http::/localhost:5173" }));
 
 app.use("/", indexRouter);
 
