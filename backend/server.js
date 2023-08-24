a59377548e2b792849a20461825f201005a42dfe
Omar Mohamed Faheem <85244203+omarmohfaheem@users.noreply.github.com>
2023-08-24 12:30:03 +0300

    Backend setup

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
+
+const PORT = 3001;
+
+const mongoURL = "mongodb://localhost:27017";
+
+mongoose
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4,
+  })
+  .then(() => {
+    console.log("Connected to Mongoose");
+  })
+  .catch((error) => {
+    console.error("Error connecting to Mongoose:", error);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
