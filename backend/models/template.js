1c6e3e51064ab958217cbbf7b34dc0413895d05e
omarmohfaheem <omarmohamedfaheem@gmail.com>
2023-08-28 06:59:34 +0300

    Created template.ejs file and updated database

--- backend/models/template.js
+++ backend/models/template.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const templateSchema = new mongoose.Schema({
   id: mongoose.Schema.Types.ObjectId,
   content: mongoose.Schema.Types.Mixed,
+  style: mongoose.Schema.Types.Mixed,
 });
 
 const Template = mongoose.model("Template", templateSchema);
