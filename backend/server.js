70052498fe462ec367768570f844f77d030da619
omarmohfaheem <omarmohamedfaheem@gmail.com>
2023-08-28 07:00:10 +0300

    some database configurations

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(express.static("public"));
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use("/", indexRouter);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/save-draft", saveDraft);
 app.use("/publish", publish);
 
