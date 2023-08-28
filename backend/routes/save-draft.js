17f92c3276572124593c0b4b87d7594f8493a6e6
omarmohfaheem <omarmohamedfaheem@gmail.com>
2023-08-28 06:59:56 +0300

    Changed save draft to send both html and css

--- backend/routes/save-draft.js
+++ backend/routes/save-draft.js
@@ -3,20 +3,21 @@ const router = express.Router();
 const Template = require("../models/template");
 
 router.post("/", async (req, res) => {
-  const content = req.body.content;
+  const { content, style } = req.body;
 
   try {
     const template = new Template({
       content,
+      style,
     });
 
-    const savedTemplate = await template.save(); // Corrected variable name
+    const savedTemplate = await template.save();
 
     res.status(201).json({
       message: "Draft saved successfully",
-      templateId: savedTemplate._id,
+      templateId: savedTemplate._id.toString(),
     });
-    console.log("Database content:", content); // Log the HTML content
+    console.log("Database content:", content, "Database style:", style);
   } catch (error) {
     console.error("Error saving draft:", error);
     res.status(500).json({ message: "Internal server error" });
