900775af97a676b04a74e8632fd57e25f849a6a8
omarmohfaheem <omarmohamedfaheem@gmail.com>
2023-09-05 19:37:04 +0300

    Updated builder to render faq content dynamically

--- src/pages/builder/Builder.jsx
+++ src/pages/builder/Builder.jsx
@@ -4,25 +4,25 @@ import gjsOptions from "/src/utils/gjsOptions";
 import { editorPlugins } from "/src/utils/plugins";
 import { defineCustomBlocks } from "/src/utils/customBlocks";
 
-import { message } from 'antd';
+import { message } from "antd";
 
 export function Builder() {
   const onEditor = (editor) => {
     const editorPanels = editor.Panels;
     const editorCommands = editor.Commands;
     const panelViews = editorPanels.addPanel({
-      id: 'views',
+      id: "views",
     });
-    
-    panelViews.get('buttons').add([
+
+    panelViews.get("buttons").add([
       {
         attributes: {
-          title: 'Open Code',
+          title: "Open Code",
         },
-        className: 'fa fa-file-code-o',
-        command: 'open-code',
-        togglable: false, 
-        id: 'open-code',
+        className: "fa fa-file-code-o",
+        command: "open-code",
+        togglable: false,
+        id: "open-code",
       },
     ]);
 
@@ -47,21 +47,23 @@ export function Builder() {
     editorCommands.add("save-db", function (editor, sender) {
       sender && sender.set("active", 0);
       editor.store();
-    
+
       const htmlContent = editor.getHtml();
-      
+
       const externalCssUrls = editor.getConfig().canvas.styles;
-      const fetchCssPromises = externalCssUrls.map(url => fetch(url).then(response => response.text()));
-    
+      const fetchCssPromises = externalCssUrls.map((url) =>
+        fetch(url).then((response) => response.text())
+      );
+
       Promise.all(fetchCssPromises)
-        .then(externalCssArray => {
+        .then((externalCssArray) => {
           const internalCss = editor.getCss();
-          const combinedCss = externalCssArray.join('\n') + '\n' + internalCss;
-    
+          const combinedCss = externalCssArray.join("\n") + "\n" + internalCss;
+
           var styles = `<style>${combinedCss}</style>`;
           console.log(htmlContent);
-          console.log('css', combinedCss);
-    
+          console.log("css", combinedCss);
+
           fetch("http://localhost:3001/save-draft", {
             method: "POST",
             headers: {
@@ -74,127 +76,138 @@ export function Builder() {
               console.log("Data saved:", data);
               localStorage.setItem("templateId", data.templateId);
               console.log("template", data.templateId);
-              message.success('Draft saved successfully');
+              message.success("Draft saved successfully");
             })
             .catch((error) => {
               console.error("Error saving data:", error);
-              message.error('Error saving draft');
+              message.error("Error saving draft");
             });
         })
         .catch((error) => {
           console.error("Error fetching external CSS:", error);
-          message.error('Error fetching external CSS');
+          message.error("Error fetching external CSS");
         });
     });
 
-    editorCommands.add("publish", 
-      function (editor, sender) {
-        sender && sender.set("active", 0);
-        editor.store();
+    editorCommands.add("publish", function (editor, sender) {
+      sender && sender.set("active", 0);
+      editor.store();
 
-        const templateId = localStorage.getItem("templateId");
-        console.log("Stored Template ID:", templateId);
+      const templateId = localStorage.getItem("templateId");
+      console.log("Stored Template ID:", templateId);
 
-        fetch(`http://localhost:3001/publish/${templateId}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
+      fetch(`http://localhost:3001/publish/${templateId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
         .then(() => {
           // Generate the link URL
           const linkUrl = `http://localhost:3001/publish/${templateId}`;
-  
-          console.log("Link to published template:", linkUrl);
-  
-          message.success('Template published successfully');
-            
-          // Use the Clipboard API to copy the link URL to the clipboard
-          navigator.clipboard.writeText(linkUrl)
-          .then(() => {
-            console.log("Link copied to clipboard:", linkUrl);
-            message.success('Link copied to clipboard');
-          })
-          .catch((error) => {
-            console.error("Error copying to clipboard:", error);
-            message.error('Error copying link to clipboard');
-          });
 
+          console.log("Link to published template:", linkUrl);
+
+          message.success("Template published successfully");
+
+          // Use the Clipboard API to copy the link URL to the clipboard
+          navigator.clipboard
+            .writeText(linkUrl)
+            .then(() => {
+              console.log("Link copied to clipboard:", linkUrl);
+              message.success("Link copied to clipboard");
+            })
+            .catch((error) => {
+              console.error("Error copying to clipboard:", error);
+              message.error("Error copying link to clipboard");
+            });
         })
         .catch((error) => {
           console.error("Error publishing:", error);
-          message.error('Error publishing template');
-        })
+          message.error("Error publishing template");
+        });
     });
 
-    
-    editor.DomComponents.addType('dynamic-api-content', {
+    // Function to fetch FAQ data
+    const fetchFAQData = (component) => {
+      const apiUrl = component.get("apiUrl");
+      if (apiUrl) {
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => {
+            // Process FAQ data and set the component's content
+            const content = generateFAQContent(data);
+            component.set("content", content);
+          })
+          .catch((error) => {
+            console.error("Error fetching FAQ data:", error);
+          });
+      }
+    };
+    // const script = (function (props) {
+    //   window.addEventListener("load", (event) => {
+    //     props.fetchFAQData;
+    //   });
+    // })();
+
+    const script = function (props) {
+      const myLibOpts = {
+        prop1: props.myprop1,
+        prop2: props.myprop2,
+      };
+      alert("My lib options: " + JSON.stringify(myLibOpts));
+    };
+
+    // Function to generate FAQ content
+    function generateFAQContent(data) {
+      // Generate HTML content for displaying FAQ items
+      let content = '<div class="faq-component">';
+      data.forEach((item) => {
+        content += `<div class="faq-item">
+          <h3>${item.question}</h3>
+          <p>${item.answer}</p>
+      </div>`;
+      });
+      content += "</div>";
+      return content;
+    }
+
+    editor.DomComponents.addType("faq-component", {
       model: {
         defaults: {
-          apiUrl: '',
-          content: '',
-          traits: [
-            {
-              type: 'text',
-              name: 'apiUrl',
-              label: 'API URL',
-            },
-            {
-              type: 'button',
-              text: 'Fetch Content',
-              full: true,
-              functionName: 'onFetchClick',
-            },
-          ],
+          script,
+          myprop1: "value1",
+          myprop2: "10",
+          apiUrl: "http://localhost:3001/faq",
+          content: "",
         },
-        methods: {
-          onFetchClick() {
-            const apiUrl = this.get('apiUrl');
-            if (apiUrl) {
-              console.log('Fetching content from:', apiUrl);
-              this.fetchContent(apiUrl);
-            }
-          },
-          fetchContent(apiUrl) {
-            fetch(apiUrl)
-              .then(response => response.text())
-              .then(data => {
-                this.set('content', data);
-              })
-              .catch(error => {
-                console.error('Error fetching content:', error);
-              });
-          },
+        init() {
+          // Fetch FAQ data when the component is initialized
+          fetchFAQData(this);
         },
       },
       view: {
         onRender() {
-          const content = this.model.get('content');
+          const content = this.model.get("content");
           if (content) {
             this.el.innerHTML = content;
           }
         },
       },
+      "script-props": ["myprop1", "myprop2"],
     });
-    editor.onReady(() => {
-      editor.getComponents().add(
-        '<link rel="stylesheet" href="public/stylesheets/canvas.css">'
-      )
-   });
-    
-    editor.BlockManager.add('dynamic-api-block', {
-      label: 'Dynamic API Block',
-      category: 'Custom Blocks',
+
+    editor.BlockManager.add("faq-block", {
+      label: "FAQ Block",
+      category: "Dynamic Blocks",
       content: {
-        type: 'dynamic-api-content',
-        components: '<p>Loading...</p>',
+        type: "faq-component",
       },
     });
 
     defineCustomBlocks(editor);
     window.editor = editor;
   };
-
 
   return (
     <>
