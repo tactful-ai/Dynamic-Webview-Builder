58737d2a1516f8a823daaeec9126bb6d200b8937
omarmohfaheem <omarmohamedfaheem@gmail.com>
2023-09-05 19:36:39 +0300

    Created example faq api

---
+++ backend/routes/faq.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+// Define two sample questions and answers
+const questionsAndAnswers = [
+  {
+    question: "What is GrapesJS?",
+    answer:
+      "GrapesJS is an open-source web page builder that allows you to create web pages visually.",
+  },
+  {
+    question: "How can I integrate an API with GrapesJS?",
+    answer:
+      "You can integrate an API with GrapesJS by creating custom components that fetch and display data from the API.",
+  },
+];
+
+router.get("/", (req, res) => {
+  res.json(questionsAndAnswers);
+});
+
+module.exports = router;
