d9c4a4d28e98f41e0bc0c811c3c5ccab837a8c75
omarmohfaheem <omarmohamedfaheem@gmail.com>
2023-09-13 16:19:29 +0300

    Removed some comments

--- backend/routes/dynamicText.js
+++ backend/routes/dynamicText.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// Sample data representing dynamic texts
 const dynamicTexts = [{ text: "Hello" }, { text: "Welcome" }];
 
-// Define a GET route that returns the dynamic texts
 router.get("/", (req, res) => {
   res.json(dynamicTexts);
 });
--- backend/routes/itemDetails.js
+++ backend/routes/itemDetails.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// Sample product data
 const products = [
   {
     id: "1",
@@ -21,7 +20,6 @@ const products = [
   },
 ];
 
-// Define a GET route that returns the product data
 router.get("/", (req, res) => {
   res.json(products);
 });
